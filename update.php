d40d493fb85fa976c031c3b0431f7f4325a31549
Meena Devi D <68017829+meena109@users.noreply.github.com>
2021-02-23 17:00:25 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,209 @@
+<?php
+
+
+
+
+$showAlert = false; 
+$showError = false; 
+$exists=false; 
+	
+if($_SERVER["REQUEST_METHOD"] == "POST") { 
+	
+	// Include file which makes the 
+	// Database Connection. 
+	include 'regdbconnect.php'; 
+	
+	$username = $_POST["username"];  
+	$phone = $_POST["phone"];	
+	$email = $_POST["email"];
+	$id = $_POST["id"];
+	$sql = "UPDATE patient SET username = '$username' , phone = '$phone', email ='$email' WHERE id = '$id'"; 
+	
+			$result = mysqli_query($conn, $sql); 
+	
+			if ($result) { 
+				$showAlert = true; 
+				header("location: indexdb.php");
+			} 
+
+		
+	
+	
+	
+}else{
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM patient WHERE id = ?";
+
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $username = $row["username"];
+                $phone = $row["phone"];
+                $email = $row["email"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+	
+	if($showAlert) { 
+	
+		echo ' <div class="alert alert-success 
+			alert-dismissible fade show" role="alert"> 
+	
+			<strong>Success!</strong> Your account is 
+			now created and you can login. 
+			<button type="button" class="close"
+				data-dismiss="alert" aria-label="Close"> 
+				<span aria-hidden="true">×</span> 
+			</button> 
+		</div> '; 
+	} 
+	
+	if($showError) { 
+	
+		echo ' <div class="alert alert-danger 
+			alert-dismissible fade show" role="alert"> 
+		<strong>Error!</strong> '. $showError.'
+	
+	<button type="button" class="close"
+			data-dismiss="alert aria-label="Close"> 
+			<span aria-hidden="true">×</span> 
+	</button> 
+	</div> '; 
+} 
+		
+	if($exists) { 
+		echo ' <div class="alert alert-danger 
+			alert-dismissible fade show" role="alert"> 
+	
+		<strong>Error!</strong> '. $exists.'
+		<button type="button" class="close"
+			data-dismiss="alert" aria-label="Close"> 
+			<span aria-hidden="true">×</span> 
+		</button> 
+	</div> '; 
+	} 
+
+?> 
+
+	<head>
+		<title>User Registration</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta content="" name="description" />
+		<meta content="" name="author" />
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+		
+	</head>
+
+	<body class="login">
+		<!-- start: REGISTRATION -->
+		<div class="row">
+			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+				<div class="logo margin-top-30">
+				<h2>Patient update records </h2>
+				</div>
+				<!-- start: REGISTER BOX -->
+				<div class="box-register">
+					<form action = "" name="registration" id="registration"  method="post">
+						<fieldset>
+						<input type = "hidden" name = "id" value =<?php echo $param_id ?> />
+							<div class="form-group">
+								<input type="text" class="form-control" name="username" placeholder='username' value =<?php echo $username ?> required></input>
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="phone" placeholder="phone" value =<?php echo $phone ?> required>
+							</div>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="email" class="form-control" name="email" id="email" onBlur="userAvailability()"  placeholder="email" value =<?php echo $email ?> required>
+									<i class="fa fa-envelope"></i> </span>
+									 <span id="user-availability-status1" style="font-size:12px;"></span>
+							</div>
+							
+								</p>
+								<button type="submit" class="btn btn-primary pull-right" id="submit" name="submit">
+									Submit <i class="fa fa-arrow-circle-right"></i>
+								</button>
+							</div>
+						</fieldset>
+					</form>
+
+			</div>
+		</div>
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/js/login.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				Login.init();
+			});
+		</script>
+		
+	<script>
+function userAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'email='+$("#email").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status1").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>	
+		
+	</body>
+	<!-- end: BODY -->
+</html>
+<?php } ?>
